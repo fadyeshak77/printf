11ac8f33952500055f37537474290058d922284b
Fady Eshak <fadyeshak5555@gmail.com>
2023-08-21 17:18:41 -0700

    digit.c

---
+++ digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * is_digit - Check if a character is a digit (0-9).
+ * @c: The character to check.
+ *
+ * Return: 1 if the character is a digit, 0 otherwise.
+ */
+int is_digit(char c)
+{
+	return (c >= '0' && c <= '9');
+}
